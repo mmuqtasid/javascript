4d6741d202fadb081c652e08290e0f5b75a7f457
mmuqtasid <96142528+mmuqtasid@users.noreply.github.com>
2023-11-23 17:33:23 +0500

    javasacript code

---
+++ variable/index.js
@@ -0,0 +1,13 @@
+let num = 10;
+let num1 = 20;
+let res = num + num1;
+console.log("Result is" + num +num1);
+console.log(`Result is  ${res}`);
+
+const number = "20";
+const number1 = 10;
+const result = number + number1;
+
+console.log(`result ${result}`)
+
+console.log(typeof(number , number1));
---
+++ variable/practice.js
@@ -0,0 +1,17 @@
+//Swap two number with out using third one;
+
+let first = 10;
+let second = 20;
+
+first = first + second;
+
+// console.log(`value of first ${first}`);
+
+second = first-second;
+
+console.log(`value of second ${second}`)
+
+first = first - second;
+
+console.log(`value of first ${first}`);
+ 
